2e6f6ade611361322e6988ea410ae9681b729869
Philip <p.sarnikowski@gmail.com>
2020-01-02 23:41:22 +0100

    Work in progress on training script. Implemented main blocks, however is untested. Logging needs refactoring. Removed some unecessary utils and requirements.

--- src/train.py
+++ src/train.py
@@ -1,173 +1,125 @@
-import yaml
-import shutil
+import os
+import datetime
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-from bert import run_classifier
-from bert import tokenization
-
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
-from datetime import datetime
-from pathlib import Path, PurePath
 
+from tensorflow import keras
+from bert import BertModelLayer
+from bert.loader import StockBertConfig, map_stock_config_to_params, load_stock_weights
+from bert.tokenization import FullTokenizer
+
+from utils_bert import convert_data, create_learning_rate_scheduler
 from utils_plotting import plot_confusion_matrix
-from utils_bert import create_examples, get_estimator, create_serving_input_receiver_fn
-from utils_bert import convert_examples_to_features
-from best_checkpoints_exporter import BestCheckpointsExporter
 
-###################
-# GLOBAL SETTINGS #
-###################
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
-log_dir = "/app/logs/{}/".format(datetime.now().strftime("%Y%m%dT%H%M%S"))
-Path(log_dir).mkdir(parents=True)
+LOG_PREFIX = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+model_dir = "/app/models/{}".format(LOG_PREFIX)
 
 logger = tf.get_logger()
-#################
-# BERT SETTINGS #
-#################
-train_batch_size = 12
-learning_rate = 2e-5
-num_train_epochs = 0.2
-max_seq_length = 192
+
+batch_size = 12
+max_learn_rate = 2e-5
+end_learn_rate = 2e-7
 warmup_proportion = 0.1
+n_epochs = 10
+max_seq_len = 256
 test_size = 0.1
+validation_split = 0.1
 random_state = None
 
-# Model files/paths
-pretrained_model_dir = "/app/models/pretrained/"
-tmp_checkpoint_dir = "/app/models/train/"
-best_checkpoint_dir = "/app/models/best_checkpoint/"
-trained_model_dir = "/app/models/trained_model/"
+pretrained_model_dir = "/app/model/".format(os.environ["BASE_MODEL_PREFIX"])
+bert_ckpt_file = os.path.join(pretrained_model_dir, "bert_model.ckpt")
+bert_config_file = os.path.join(pretrained_model_dir, "bert_config.json")
 
-bert_vocab = pretrained_model_dir + "vocab.txt"
-bert_config_path = pretrained_model_dir + "bert_config.json"
+bert_vocab_file = os.path.join(pretrained_model_dir, "vocab.txt")
+
 do_lower_case = "uncased" in pretrained_model_dir
-init_checkpoint = pretrained_model_dir + "bert_model.ckpt"
+tokenizer = FullTokenizer(vocab_file=bert_vocab_file, do_lower_case=do_lower_case)
 
-Path(best_checkpoint_dir).mkdir(exist_ok=True)
-
-save_checkpoints_steps = 2000
-save_summary_steps = 250
-keep_checkpoint_max = 3
-################
-# DATA LOADING #
-################
 df = pd.read_csv("/app/data/stack-overflow-data.csv")
 df_train, df_test = train_test_split(df, test_size=test_size, random_state=random_state)
-df_train, df_eval = train_test_split(df_train, test_size=test_size, random_state=random_state)
 
 label_encoder = preprocessing.LabelEncoder()
 label_encoder.fit(df["tags"])
+num_classes = len(label_encoder.classes_)
 np.save(pretrained_model_dir + "label_encoder.npy", label_encoder.classes_)
 
 y_train = label_encoder.transform(df_train["tags"])
-y_eval = label_encoder.transform(df_eval["tags"])
 y_test = label_encoder.transform(df_test["tags"])
 
-num_labels = len(label_encoder.classes_)
-label_list = [str(num) for num in range(num_labels)]
-
-tokenizer = tokenization.FullTokenizer(vocab_file=bert_vocab, do_lower_case=do_lower_case)
-
-tf.compat.v1.logging.info("***** Converting data to BERT format... *****")
-
 tf.compat.v1.logging.info("***** Coverting training examples *****")
-train_examples = create_examples(df_train["post"], y_train)
-train_features = convert_examples_to_features(
-    train_examples, label_list, max_seq_length, tokenizer
-)
-
-tf.compat.v1.logging.info("***** Converting evaluation examples *****")
-eval_examples = create_examples(df_eval["post"], y_eval)
-eval_features = convert_examples_to_features(eval_examples, label_list, max_seq_length, tokenizer)
-
+x_train = convert_data(df_train["post"], tokenizer=tokenizer, max_seq_len=max_seq_len)
 tf.compat.v1.logging.info("***** Converting test examples *****")
-test_examples = create_examples(df_test["post"], y_test)
-test_features = convert_examples_to_features(test_examples, label_list, max_seq_length, tokenizer)
-
+x_test = convert_data(df_test["post"], tokenizer=tokenizer, max_seq_len=max_seq_len)
 tf.compat.v1.logging.info("***** ...Finished converting data to BERT format *****")
-#############
-# INIT BERT #
-#############
-num_train_steps = int(len(train_examples) / train_batch_size * num_train_epochs)
-num_warmup_steps = int(num_train_steps * warmup_proportion)
 
-model_config = {
-    "model_dir": tmp_checkpoint_dir,
-    "save_summary_steps": save_summary_steps,
-    "save_checkpoints_steps": save_checkpoints_steps,
-    "keep_checkpoint_max": keep_checkpoint_max,
-    "bert_config_path": bert_config_path,
-    "num_labels": num_labels,
-    "init_checkpoint": init_checkpoint,
-    "learning_rate": learning_rate,
-    "num_train_steps": num_train_steps,
-    "num_warmup_steps": num_warmup_steps,
-    "train_batch_size": train_batch_size,
-    "max_seq_length": max_seq_length,
-    "do_lower_case": do_lower_case,
-    "vocab_file": bert_vocab,
-}
-estimator = get_estimator(**model_config)
-############
-# TRAINING #
-############
-train_input_fn = run_classifier.input_fn_builder(
-    features=train_features, seq_length=max_seq_length, is_training=True, drop_remainder=False
+
+def create_model(max_seq_len, summary=False):
+    with tf.io.gfile.GFile(bert_config_file, "r") as reader:
+        bc = StockBertConfig.from_json_string(reader.read())
+        bert_params = map_stock_config_to_params(bc)
+        bert = BertModelLayer.from_params(bert_params, name="bert")
+
+    input_ids = keras.layers.Input(shape=(max_seq_len,), dtype="int32", name="input_ids")
+    output = bert(input_ids)
+
+    cls_out = keras.layers.Lambda(lambda seq: seq[:, 0, :])(output)
+    cls_out = keras.layers.Dropout(0.5)(cls_out)
+    logits = keras.layers.Dense(units=768, activation="relu")(cls_out)
+    logits = keras.layers.Dropout(0.5)(logits)
+    logits = keras.layers.Dense(units=num_classes, activation="softmax")(logits)
+
+    model = keras.Model(inputs=input_ids, outputs=logits)
+    model.build(input_shape=(None, max_seq_len))
+
+    load_stock_weights(bert, bert_ckpt_file)
+
+    model.compile(
+        optimizer=keras.optimizers.Adam(),
+        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[keras.metrics.SparseCategoricalAccuracy(name="acc")],
+    )
+    if summary:
+        model.summary()
+    return model
+
+
+model = create_model(max_seq_len, summary=True)
+tensorboard_callback = keras.callbacks.TensorBoard(log_dir=model_dir)
+
+learning_rate_callback = create_learning_rate_scheduler(
+    max_learn_rate=max_learn_rate,
+    end_learn_rate=end_learn_rate,
+    warmup_proportion=warmup_proportion,
+    n_epochs=n_epochs,
 )
-train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn, max_steps=num_train_steps)
-
-best_exporter = BestCheckpointsExporter(
-    serving_input_receiver_fn=create_serving_input_receiver_fn(max_seq_length)
+early_stopping_callback = keras.callbacks.EarlyStopping(
+    monitor="val_loss", patience=3, restore_best_weights=True
 )
-
-eval_input_fn = run_classifier.input_fn_builder(
-    features=eval_features, seq_length=max_seq_length, is_training=False, drop_remainder=False
+model.fit(
+    x_train,
+    y_train,
+    validation_split=validation_split,
+    batch_size=batch_size,
+    epochs=n_epochs,
+    callbacks=[learning_rate_callback, early_stopping_callback, tensorboard_callback],
 )
+model.save_weights("{}/model.h5".format(model_dir))
 
-eval_spec = tf.estimator.EvalSpec(
-    input_fn=eval_input_fn, steps=None, exporters=best_exporter, throttle_secs=50
-)
+model = create_model(max_seq_len)
+model.load_weights("{}/model.h5".format(model_dir))
 
-start_time = datetime.now()
-tf.compat.v1.logging.info("***** Running training *****")
-tf.compat.v1.logging.info("Number of examples = {}".format(len(train_examples)))
-tf.compat.v1.logging.info("Batch size = {}".format(train_batch_size))
-tf.compat.v1.logging.info("Num of steps = {}".format(num_train_steps))
-
-tf.estimator.train_and_evaluate(estimator, train_spec=train_spec, eval_spec=eval_spec)
-
-tf.compat.v1.logging.info(
-    "***** Finished training (took: {}) *****".format(datetime.now() - start_time)
-)
-########################
-# LOAD BEST CHECKPOINT #
-########################
-model_config.update({"model_dir": best_checkpoint_dir})
-with open(best_checkpoint_dir + "checkpoint") as file:
-    checkpoint = yaml.safe_load(file)
-model_config.update({"init_checkpoint": best_checkpoint_dir + checkpoint["model_checkpoint_path"]})
-estimator = get_estimator(**model_config)
-###################
-# MODEL EVALUATION #
-####################
-test_input_fn = run_classifier.input_fn_builder(
-    features=test_features, seq_length=max_seq_length, is_training=False, drop_remainder=False
-)
-predictions = estimator.predict(input_fn=test_input_fn)
-
-y_pred = []
-for prediction in predictions:
-    y_pred.append(np.argmax(prediction["probabilities"]))
+predictions = model.predict(x_test, batch_size=batch_size)
+y_pred = np.argmax(predictions)
 tf.compat.v1.logging.info(
     "***** BERT TEST ACCURACY: {} *****".format(accuracy_score(y_test, y_pred))
 )
-
 _, counts = np.unique(y_test, return_counts=True)
 
 matrix = plot_confusion_matrix(
@@ -176,30 +128,5 @@ matrix = plot_confusion_matrix(
     ["{} ({:d})".format(s, c) for s, c in zip(label_encoder.classes_, counts)],
     figsize=(16, 14),
     normalize=True,
-    save_path=log_dir + "confusion_matrix.png",
+    save_path=model_dir + "confusion_matrix.png",
 )
-tf.compat.v1.logging.info(
-    "\n" + classification_report(y_test, y_pred, target_names=label_encoder.classes_)
-)
-################
-# EXPORT MODEL #
-################
-estimator.export_saved_model(
-    trained_model_dir, create_serving_input_receiver_fn(max_seq_length=max_seq_length)
-)
-trained_model_dir = [d.as_posix() for d in Path(trained_model_dir).glob("*/") if d.is_dir()][0]
-model_config.update({"trained_model_dir": trained_model_dir})
-with open(pretrained_model_dir + "model_config.yml", "w") as file:
-    yaml.dump(model_config, file)
-###########
-# CLEANUP #
-###########
-# Copy logs
-for filepath in Path(tmp_checkpoint_dir).glob("**/events*"):
-    shutil.copy(filepath, PurePath(log_dir).joinpath(filepath.name))
-# Remove train dir and best checkpoint dir
-shutil.rmtree(tmp_checkpoint_dir)
-shutil.rmtree(best_checkpoint_dir)
-# Remove pretrained model
-for filepath in Path(pretrained_model_dir).glob("bert_model.ckpt.*"):
-    filepath.unlink()
